79f5357564cb2570eb0c4342ea224c6b0642fc04
Donovan <donovan.persent@gmail.com>
2020-01-24 14:33:50 +0100

    Create DropContainer and its editor + begin harvestable

---
+++ Assets/Scripts/Interactables/Harvestable.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using LateUpdate.Actions;
+using UnityEngine;
+
+namespace LateUpdate {
+    [RequireComponent(typeof(DropContainer))]
+    public class Harvestable : Interactable
+    {
+        [SerializeField] Item[] requiredItems;
+
+        public DropContainer DropContainer { get; protected set; }
+
+        public override List<GameAction> GetPossibleActions(Actor actor)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void Awake()
+        {
+            base.Awake();
+            DropContainer = GetComponent<DropContainer>();
+        }
+    }
+}
---
+++ Assets/Scripts/Items/DropChance.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System;
+
+namespace LateUpdate {
+    [Serializable]
+    public struct DropChance
+    {
+        public Item item;
+        [Range(0.001f, 1)] public float weight;
+    }
+}
